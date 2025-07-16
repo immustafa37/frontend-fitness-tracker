a825a238109b0c2b5101ca99bb73c04379fdc089
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-15 20:26:02 -0700

    updated file App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { ThemeProvider } from './context/ThemeContext';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
+import Diet from './pages/Diet/Diet';
+import Exercise from './pages/Exercise/Exercise';
+import Fasting from './pages/Fasting/Fasting';
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="app">
-        <Home />
-      </div>
+      <Router>
+        <div className="app">
+          <nav className="navbar">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/diet" className="nav-link">Diet</Link>
+            <Link to="/exercise" className="nav-link">Exercise</Link>
+            <Link to="/fasting" className="nav-link">Fasting</Link>
+          </nav>
+          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/diet" element={<Diet />} />
+            <Route path="/exercise" element={<Exercise />} />
+            <Route path="/fasting" element={<Fasting />} />
+          </Routes>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 }
