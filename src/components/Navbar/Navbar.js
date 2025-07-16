0083d59f7702c34fb275254194efa1432ccc5eac
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-15 22:12:33 -0700

    updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import NeumoButton from '../NeumoButton/NeumoButton';
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">HealthTracker</Link>
+    <>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <button className="menu-toggle" onClick={toggleSidebar}>
+            {isSidebarOpen ? <FaTimes /> : <FaBars />}
+          </button>
+          <Link to="/">HealthTracker</Link>
+        </div>
+        <div className="navbar-theme">
+          <ThemeToggle />
+        </div>
+      </nav>
+
+      <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+        <div className="sidebar-links">
+          <Link to="/dashboard" onClick={() => setIsSidebarOpen(false)}>
+            <NeumoButton className="sidebar-button">Dashboard</NeumoButton>
+          </Link>
+          <Link to="/activities" onClick={() => setIsSidebarOpen(false)}>
+            <NeumoButton className="sidebar-button">Activities</NeumoButton>
+          </Link>
+          <Link to="/nutrition" onClick={() => setIsSidebarOpen(false)}>
+            <NeumoButton className="sidebar-button">Nutrition</NeumoButton>
+          </Link>
+          <Link to="/settings" onClick={() => setIsSidebarOpen(false)}>
+            <NeumoButton className="sidebar-button">Settings</NeumoButton>
+          </Link>
+        </div>
       </div>
-      <div className="navbar-links">
-        <Link to="/dashboard"><NeumoButton>Dashboard</NeumoButton></Link>
-        <Link to="/activities"><NeumoButton>Activities</NeumoButton></Link>
-        <Link to="/nutrition"><NeumoButton>Nutrition</NeumoButton></Link>
-        <Link to="/settings"><NeumoButton>Settings</NeumoButton></Link>
-      </div>
-      <div className="navbar-theme">
-        <ThemeToggle />
-      </div>
-    </nav>
+
+      {/* Overlay when sidebar is open */}
+      {isSidebarOpen && (
+        <div className="sidebar-overlay" onClick={toggleSidebar}></div>
+      )}
+    </>
   );
 };
 
